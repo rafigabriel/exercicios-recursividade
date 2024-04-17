f7d51e4b80c488b5fd44304d0c8460bf9e850c2a
Rafi Gabriel <grafi@grupobel.local>
2024-04-17 16:50:31 -0300

    Oi

---
+++ src/ex007.java
@@ -0,0 +1,15 @@
+public class ex007 {
+    public static void main(String[] args) {
+        String frases [] = {"radar"};
+    }
+    
+    public static boolean verifPalindromo(String frases[]){
+        if(frases.length == 0){
+            return false;
+        }  
+
+        String reverso 
+
+
+    }
+}
